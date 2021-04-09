0c696e8bcb7f78836426b2a9baff167726c0ff43
MirrorCub3 <joycemai.exe@gmail.com>
2021-04-09 09:32:52 -0700

    changes from last night

--- index5.js
+++ index5.js
@@ -14,8 +14,10 @@ let playerSetNum = 1; // set this to the player num when game is created
 let handNum = 1;
 
 let gameName = "Virtual Cards";
+let deckSize = 52;
 
 app.get("/",function(req,res) {
+    playerCount = 0;
     res.sendFile(__dirname + "/public/views/index.html");
 });
 app.get("/player",function(req,res) {
@@ -27,60 +29,17 @@ app.get("/player",function(req,res) {
     }
 });
 app.post("/create",function(req,res) {
-  playerCount = 0;
-  playerSetNum = req.body.playernum;
-  handNum = req.body.handnum;
+    if(req.body.playernum * req.body.handnum > deckSize){
+        alert("Invalid Starting Hand: Not Enough Cards");
+        return;
+    }
+    playerSetNum = req.body.playernum;
+    handNum = req.body.handnum;
 
-  gameName = req.body.name;
-  res.json(null);
+    gameName = req.body.name;
+    res.json(null);
 });
-// app.get("/typeCheck", function(req, res){
-//     console.log("server typeCheck = " + calcType);
-//
-//     res.json({type:calcType});
-// });
-// app.get("/typeChange", function(req, res){
-//     if (req.query.type == 'Area')
-//     {
-//         calcType = 0;
-//     }
-//     else
-//     {
-//         calcType = 1;
-//     }
-//     console.log("server typeChange = " + calcType);
-//     res.json({});
-// });
-//
-// app.get("/calculate", function(req, res){
-//     console.log(req.query.len1 + " " + req.query.len2 + " " + req.query.shape);
-//     if (calcType == 0)
-//     {
-//         if (req.query.shape == 'square')
-//         {
-//             let area = req.query.len1 * req.query.len1;
-//             res.json({type:0,val:area});
-//         }
-//         else
-//         {
-//             let area = req.query.len1 * req.query.len2;
-//             res.json({type:0,val:area});
-//         }
-//     }
-//     else
-//     {
-//         if (req.query.shape == 'square')
-//         {
-//             let perim = 4*req.query.len1;
-//             res.json({type:1,val:perim});
-//         }
-//         else
-//         {
-//             let perim = 2*(parseInt(req.query.len1) + parseInt(req.query.len2));
-//             res.json({type:1,val:perim});
-//         }
-//     }
-// });
-app.listen(4005,function(){
-    console.log("started on port 4005");
+
+app.listen(3000,function(){
+    console.log("started on port 3000");
 });
