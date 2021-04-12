ce8476580255d62210fbd6c3ee03728853312728
MirrorCub3 <joycemai.exe@gmail.com>
2021-04-12 02:54:39 -0700

    Working player ID and chat
    changes
    - created a player array (playerList) - use true and false as active checks
    - dynamically add players - new ones can take over now opened spots
    - the name of the game can be updated dynamically
    - active player counter on the settings page
    - chatboxes now communicate across separate tabs

--- index5.js
+++ index5.js
@@ -11,35 +11,85 @@ app.use(express.static("./public"));
  // settings variables
 let playerCount = 0;
 let playerSetNum = 0; // set this to the player num when game is created
+let playerList = [];
+let numActive = 0;
 let handNum = 1;
 
 let gameName = "Virtual Cards";
 let deckSize = 52;
 
+let chat = [];
+
 app.get("/",function(req,res) {
-    playerCount = 0;
+    numActive = 0;
+    chat.length = 0;
     res.sendFile(__dirname + "/public/views/index.html");
 });
 app.get("/player",function(req,res) {
-    if(playerCount >= playerSetNum){
+    if(numActive >= playerSetNum){
         res.end("Error! Player Count Exceeded");}
     else{
         res.sendFile(__dirname + "/public/views/player.html");
-        playerCount += 1;
+        numActive++;
+        let indexFound = false;
+        for(let i = 0; i < playerList.length; i++){
+            if(playerList[i] == false && indexFound == false){
+                playerList[i] = true;
+                playerId = ++i;
+                indexFound = true;
+            }
+        }
+    }
+});
+app.get("/player2",function(req,res) {
+      let ident = req.query.id;
+      if(req.query.index == 1){
+          ident = playerId;
+          res.json({id:ident,gamename:gameName});
+      }
+});
+app.get("/indexCheck",function(req,res) {
+    let numTrue = 0;
+    for(let i = 0; i < playerList.length; i++){
+        // console.log("player " + i + " " + playerList[i]);
+        if(playerList[i] == true)
+            numTrue++;
+    }
+    numActive = numTrue;
+    //console.log("numActive " + numActive);
+    res.json({active:numActive});
+});
+app.get("/checkplayer", function(req,res){
+    if(req.query.active == 0){
+      numActive--;
+      playerList[req.query.id - 1] = false; // player inactive
+    }
+    else if(req.query.active == 1){
+      playerList[req.query.id - 1] = true; // player is active
+      res.json({gamename:gameName,chat:chat});
     }
 });
 app.post("/create",function(req,res) {
     if(req.body.playernum * req.body.handnum > deckSize){
-        alert("Invalid Starting Hand: Not Enough Cards");
+        res.json({error:1});
         return;
     }
     playerSetNum = req.body.playernum;
+
+    playerList.length = playerSetNum;
+    for(let i = 0; i<playerList.length; i++){
+        playerList[i] = false;
+    }
     handNum = req.body.handnum;
-
+    //clears chat
+    chat.length = 0;
     gameName = req.body.name;
-    res.json(null);
+    res.json({error:0});
 });
-
-app.listen(3000,function(){
-    console.log("started on port 3000");
+app.post("/chat",function(req,res) {
+    chat[chat.length] = req.body.line;
+    res.json({chat:chat});
+});
+app.listen(4005,function(){
+    console.log("started on port 4005");
 });
--- public/js/player.js
+++ public/js/player.js
@@ -1,6 +1,37 @@
 // client js
+let id = 1;
 function SendMessage(){
-let chat = (document.getElementById("nameset").value + ": " + document.getElementById("sendMessage").value )
-document.getElementById("chatbox").innerHTML +=  chat + "\n"
-document.getElementById("sendMessage").value = ""
+    let chat = (document.getElementById("nameset").value + ": " + document.getElementById("sendMessage").value );
+    $.post("/chat", {line:chat},function(data){
+      console.log(data.chat);
+      //document.getElementById("chatbox").innerHTML +=  chat + "\n";
+      document.getElementById("sendMessage").value = "";
+    });
+}
+function sucessInfo(data){
+    if(!data)
+        return;
+    id = data.id;
+    document.getElementById("nameset").value = "Player " + id;
+    document.getElementById("playerId").innerHTML = "PLAYER " + id;
+    document.getElementById("gameName").innerHTML = data.gamename;
+}
+$(document).ready(function(){
+    console.log("player ready");
+    $.get("/player2", {index:1,id:id},sucessInfo);
+});
+window.addEventListener('beforeunload',function () {
+    $.get("/checkplayer", {active:0,id:id},null);
+});
+playerCheck();
+function playerCheck() {
+    $.get("/checkplayer", {active:1,id:id},function(data){
+        document.getElementById("gameName").innerHTML = data.gamename;
+        document.getElementById("chatbox").innerHTML = "";
+        for(let i = 0; i < data.chat.length;i++){
+            document.getElementById("chatbox").innerHTML +=  data.chat[i] + "\n";
+        }
+    });
+    let numMilliSeconds = 500;
+    setTimeout(playerCheck, numMilliSeconds);
 }
