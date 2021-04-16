7e4ce7460cc631959240dd5dbe9e2d085b21116a
MirrorCub3 <joycemai.exe@gmail.com>
2021-04-16 06:59:20 -0700

    still working on it
     - need to fix the starting hand and the adding card drawing

--- playerObject.js
+++ playerObject.js
@@ -1,8 +1,19 @@
 module.exports = class Player {
-    constructor() {
+    constructor( id ) {
+        this.active = false;
         this.hand = [];
-        this.realId = 0;
-        this.id = ++this.realId;
+        this.realId = id;
+        this.id = this.realId + 1;
         this.name = "Player " + this.id;
+    }
+    defaultState(id){
+        this.active = false;
+        this.hand = [];
+        this.realId = id;
+        this.id = this.realId + 1;
+        this.name = "Player " + this.id;
+    }
+    setHand(arr){
+
     }
 }
