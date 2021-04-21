332705c9a3ae418ec1bdc3cb0d814831b2815ad4
wcf <54386460+trappedintokyo@users.noreply.github.com>
2021-04-21 09:50:27 -0700

    Update index65.js

--- index65.js
+++ index65.js
@@ -11,7 +11,6 @@ app.use(express.static("./public"));
 
 app.use(routes);
 
-let port = process.env.PORT || 4065;
-app.listen(port,function(){
-    console.log("started on port " + port);
+app.listen(4065,function(){
+    console.log("started on port " + 4065);
 });
