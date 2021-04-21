3240df0840b5a94b7718d93ef69c5ea15274cee9
wcf <54386460+trappedintokyo@users.noreply.github.com>
2021-04-21 09:51:10 -0700

    Update index85.js

--- index85.js
+++ index85.js
@@ -11,7 +11,7 @@ app.use(express.static("./public"));
 
 app.use(routes);
 
-let port = process.env.PORT || 4085;
-app.listen(port,function(){
-    console.log("started on port " + port);
+
+app.listen(4085,function(){
+    console.log("started on port " + 4085);
 });
