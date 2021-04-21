c5e02ac5f06203f901ca3270440480263ac5fb9a
wcf <54386460+trappedintokyo@users.noreply.github.com>
2021-04-21 09:49:35 -0700

    Update index5.js

--- index5.js
+++ index5.js
@@ -11,7 +11,7 @@ app.use(express.static("./public"));
 
 app.use(routes);
 
-let port = process.env.PORT || 4005;
-app.listen(port,function(){
-    console.log("started on port " + port);
+
+app.listen(4005,function(){
+    console.log("started on port " + 4005);
 });
